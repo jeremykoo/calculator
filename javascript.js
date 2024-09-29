c3c4fb7bf0a014167a4d0f2ae212c634a07b765a
Jeremy Koo <38170465+jeremykoo@users.noreply.github.com>
2024-09-28 17:39:52 -0700

    update display with numbers

--- javascript.js
+++ javascript.js
@@ -1,9 +1,22 @@
+
 const display = document.querySelector(".display");
-const allButtons = document.querySelectorAll("button");
 const buttonAC = document.querySelector(".clear");
+const numbers = document.querySelectorAll(".number");
 
 buttonAC.addEventListener("click", () => {
     display.textContent = 0;
     console.log("clicked AC");
 });
 
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        // if display is 0 and text is 0 do nothing
+        if (!(display.textContent == "0" && number.textContent == "0")) {
+            if (display.textContent == "0" && number.textContent != "0") {
+                display.textContent = number.textContent;
+            } else {
+                display.textContent += number.textContent;
+            }
+        }
+    });
+});
